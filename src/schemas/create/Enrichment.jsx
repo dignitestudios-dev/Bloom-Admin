aa52c35ae8d8e062216d18f653592753edfedd15
Hammad ul haq <120801517+Hammadulhaq123@users.noreply.github.com>
2024-09-27 16:25:31 +0500

    validation from enrichement removed

--- src/schemas/create/Enrichment.jsx
+++ src/schemas/create/Enrichment.jsx
@@ -1,5 +1,5 @@
 import * as Yup from "yup";
 
 export const enrichmentcreate = Yup.object({
-  videoUrl: Yup.string().min(3).max(30).required("Please enter video url."),
+  videoUrl: Yup.string().min(3).required("Please enter video url."),
 });
