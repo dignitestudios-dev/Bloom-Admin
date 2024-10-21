15538f7f1e68ff33db0f9196b665f9572bb78977
JackAnderson01 <120801517+Hammadulhaq123@users.noreply.github.com>
2024-10-21 19:17:22 +0500

    Playlist cover file added

--- src/components/create post/BrittanyPlaylistCreatePost.jsx
+++ src/components/create post/BrittanyPlaylistCreatePost.jsx
@@ -23,14 +23,14 @@ const BrittanyPlaylistCreatePost = ({ id }) => {
   const { baseUrl, setError, setSuccess } = useContext(AppContext);
   const [loading, setLoading] = useState(false);
 
-  const formatDuration = (durationInSeconds) => {
-    const minutes = Math.floor(durationInSeconds / 60);
-    const seconds = Math.floor(durationInSeconds % 60);
-    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
-      2,
-      "0"
-    )} mins`;
-  };
+  // const formatDuration = (durationInSeconds) => {
+  //   const minutes = Math.floor(durationInSeconds / 60);
+  //   const seconds = Math.floor(durationInSeconds % 60);
+  //   return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+  //     2,
+  //     "0"
+  //   )} mins`;
+  // };
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -45,37 +45,37 @@ const BrittanyPlaylistCreatePost = ({ id }) => {
     }
   };
 
-  const handleAudioChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setAudioBase(reader.result);
-      };
-      reader.readAsDataURL(file);
-      setAudioError(false);
-      setAudio(file);
+  // const handleAudioChange = (e) => {
+  //   const file = e.target.files[0];
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       setAudioBase(reader.result);
+  //     };
+  //     reader.readAsDataURL(file);
+  //     setAudioError(false);
+  //     setAudio(file);
 
-      // Create an audio element to calculate duration
-      const audioElement = document.createElement("audio");
-      audioElement.src = URL.createObjectURL(file);
-      audioElement.onloadedmetadata = () => {
-        const durationInSeconds = audioElement.duration;
-        setAudioDuration(formatDuration(durationInSeconds)); // Set the formatted audio duration
-      };
-    }
-  };
+  //     // Create an audio element to calculate duration
+  //     const audioElement = document.createElement("audio");
+  //     audioElement.src = URL.createObjectURL(file);
+  //     audioElement.onloadedmetadata = () => {
+  //       const durationInSeconds = audioElement.duration;
+  //       setAudioDuration(formatDuration(durationInSeconds)); // Set the formatted audio duration
+  //     };
+  //   }
+  // };
 
   const handleRemoveImage = () => {
     setImages(null);
     setImageBase(null);
   };
 
-  const handleRemoveAudio = () => {
-    setAudio(null);
-    setAudioBase(null);
-    setAudioDuration(null); // Reset audio duration
-  };
+  // const handleRemoveAudio = () => {
+  //   setAudio(null);
+  //   setAudioBase(null);
+  //   setAudioDuration(null); // Reset audio duration
+  // };
 
   const { values, handleBlur, handleChange, handleSubmit, errors, touched } =
     useFormik({
@@ -86,20 +86,19 @@ const BrittanyPlaylistCreatePost = ({ id }) => {
         const headers = {
           Authorization: `Bearer ${Cookies.get("token")}`,
         };
+        const formdata = new FormData();
 
+        formdata.append("url", values.url?.trim());
+        formdata.append("categoryId", id);
+        formdata.append("cover", images);
         try {
-          await axios.post(
-            `${baseUrl}/api/brittanyPlaylist`,
-            {
-              url: values.url?.trim(),
-              categoryId: id,
-            },
-            {
-              headers,
-            }
-          );
+          await axios.post(`${baseUrl}/api/brittanyPlaylist`, formdata, {
+            headers,
+          });
           setSuccess("Playlist Updated Successfully.");
           values.url = "";
+          setImageBase(null);
+          setImages(null);
           setLoading(false);
         } catch (error) {
           setError(error?.response?.data?.message);
@@ -115,6 +114,45 @@ const BrittanyPlaylistCreatePost = ({ id }) => {
     >
       <div className="w-full h-32 rounded-2xl flex justify-center items-center text-3xl font-bold bg text-white">
         Brittany's Playlist
+      </div>
+
+      <div className="w-full flex flex-col rounded-2xl gap-2 justify-start items-start">
+        <div
+          className={`w-full h-40 rounded-2xl cursor-pointer bg-gray-50 flex flex-col gap-1 justify-center items-center ${
+            imageError ? "border border-red-500" : ""
+          }`}
+          onClick={() =>
+            document.getElementById("attraction-image-add").click()
+          }
+        >
+          <input
+            id="attraction-image-add"
+            className="w-full hidden h-24 rounded-full text-sm outline-none border-none px-4"
+            type="file"
+            accept="image/png, image/jpeg"
+            onChange={handleImageChange}
+          />
+          {imageBase ? (
+            <img
+              src={imageBase}
+              alt=""
+              className="w-full h-full object-contain"
+            />
+          ) : (
+            <div className="w-full h-full flex flex-col justify-center items-center">
+              <CiImageOn className="text-3xl text-gray-600 font-medium" />
+              <span className="text-md font-bold text-gray-600">
+                Playlist Cover Photo
+              </span>
+              <span className="text-xs font-medium text-gray-600">
+                Please provide the image in jpg or png format.
+              </span>
+            </div>
+          )}
+        </div>
+        {imageError && (
+          <p className="text-red-700 text-sm font-medium">{imageError}</p>
+        )}
       </div>
 
       <div className="w-full h-auto flex flex-col justify-start items-start gap-[2px]">
